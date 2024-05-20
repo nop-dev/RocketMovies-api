44c6414cf2b545461fd4fe70d6e5aa6e318328f7
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-05-20 15:22:34 -0300

    feat: :sparkles: adiciona método para cadastro de usuário (sem database)

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,21 @@
+const { hash } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+
 class UsersController {
     async create(req, res) {
+        const { name, email, password } = req.body;
+
+        if(!name) {
+            throw new AppError("O nome é obrigatório...")
+        }
+
+        console.log(name)
+
+        const hashedPassword = await hash(password, 12)
         
+        res.status(201).json();
+
+        return res.status(201).json();
     }
 }
 
