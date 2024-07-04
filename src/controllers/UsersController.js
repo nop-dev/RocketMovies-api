624e762e2b3e4d192e7e144dca5024adc1bf7f6f
Yuri Sousa <ysousa.dev@gmail.com>
2024-07-04 11:53:29 -0300

    feat: :sparkles: adiciona função pra o user modificar a senha

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,34 +1,68 @@
-knex = require("../database/knex");
-const { hash } = require("bcryptjs");
+const knex = require("../database/knex");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
 class UsersController {
     async create(req, res) {
         const { name, email, password } = req.body;
 
-        if(!name) {
-            throw new AppError("O nome é obrigatório...")
+        if (!name) {
+            throw new AppError("O nome é obrigatório...");
         }
 
-        const hashedPassword = await hash(password, 12)
-        
-        const createUser = await knex("users").insert({
-            name : name,
-            email : email,
-            password : hashedPassword
-        })
-        
+        const hashedPassword = await hash(password, 8);
+
+        await knex("users").insert({
+            name: name,
+            email: email,
+            password: hashedPassword
+        });
+
         res.status(201).json();
     }
 
     async update(req, res) {
-        const { name, email, password, old_password } = res.body;
-        const { id } = res.params;
+        let { name, email, new_password, old_password } = req.body;
+        const { id } = req.params;
 
-        if(!user) {
+        const user = await knex('users').where({ id: id }).first();
+
+        if (!user) {
             throw new AppError("Usuário não encontrado...");
-        };
+        }
+
+        if (email !== user.email) {
+            const userWithUpdatedEmail = await knex('users').where({ email: email }).first();
+            if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+                throw new AppError("Este email já está em uso...");
+            }
+        }
+
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        if (new_password && !old_password) {
+            throw new AppError("Você precisa informar a senha antiga para definir uma nova senha...");
+        }
+
+        if (new_password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password);
+            if (!checkOldPassword) {
+                throw new AppError("A senha antiga não está correta.");
+            }
+
+            const updatedpassword = await hash(new_password, 8);
+            user.password = updatedpassword;
+        }
+
+        await knex("users").where({ id: id }).update({
+            name: user.name,
+            email: user.email,
+            password: user.password
+        });
+
+        res.status(200).json({ message: "Usuário atualizado com sucesso." });
     }
 }
 
-module.exports = UsersController
+module.exports = UsersController;
